8342b8f0a6c6d05f951eed230259a1417c9633fc
Geetansh Gupta <67698473+geetansh73@users.noreply.github.com>
2021-03-07 13:53:04 +0530

    Add files via upload

---
+++ swap without 3rd variable.java
@@ -0,0 +1,15 @@
+<!Doctype html>
+<html>
+  <head>
+    <script>
+ var a=parseInt(prompt());
+ var b=parseInt(prompt());
+  a=a+b;
+  b=a-b;
+  a=a-b;
+  document.write(a," ",b);
+        </script>
+    </head>
+    </body>
+    </body>
+    </html>
